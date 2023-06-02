c0147e9f76ff2db4ab84b37e1dd8dce15270720a
goappycodes <hello@appycodes.com>
2023-06-03 02:22:39 +0530

    Synth Wave Art

---
+++ Assets/Scripts/AudioVisualiser.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (AudioSource))]
+public class AudioVisualiser : MonoBehaviour
+{
+    AudioSource _audioSource;
+    public static float[] _samples = new float[512];
+    public static float[] _freqBands= new float[8];
+    public static float[] _bandBuffer= new float[8];
+    float[] _bufferDecrease = new float[8];
+
+    public static float[] _audioBand = new float[8];
+    public static float[] _audioBandBuffer = new float[8];
+    float[] _frequencyBandHighest = new float[8];
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getSpectrumFromAudioSource();
+        MakeFrequencyBands();
+        BandBuffer();
+        CreateAudioBands();
+    }
+
+    void CreateAudioBands()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            if(_freqBands[i] > _frequencyBandHighest[i])
+            {
+                _frequencyBandHighest[i] = _freqBands[i];
+
+            }
+            _audioBand[i] = (_freqBands[i] / _frequencyBandHighest[i]);
+            _audioBandBuffer[i] = (_bandBuffer[i] / _frequencyBandHighest[i]);
+
+
+        }
+    }
+
+    void getSpectrumFromAudioSource()
+    {
+        _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+    }
+
+    void BandBuffer()
+    {
+        for (int g = 0; g < 8; ++g)
+        {
+            if(_freqBands[g] > _bandBuffer[g])
+            {
+                _bandBuffer[g] = _freqBands[g];
+                _bufferDecrease[g] = 0.005f;
+            }
+            if (_freqBands[g] < _bandBuffer[g])
+            {
+                _bandBuffer[g] -= _bufferDecrease[g];
+                _bufferDecrease[g] *= 1.2f;
+            }
+        }
+    }
+
+    void MakeFrequencyBands()
+    {
+        int count = 0;
+        for (int i = 0; i< 8; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+            if (i == 7)
+            {
+                sampleCount += 2;
+            }
+            for(int j = 0; j < sampleCount; j ++)
+            {
+                average += _samples[count] * (count + 1);
+                count++;
+            }
+            average /= count;
+            _freqBands[i] = average * 10;
+        }
+    }
+}
---
+++ Assets/Scripts/ParamCube.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParamCube : MonoBehaviour
+{
+    public int _band;
+    public float _startScale, _scaleMultiplier;
+    public bool _useBuffer;
+    Material material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<MeshRenderer>().materials[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_useBuffer)
+        {
+/*            transform.localScale = new Vector3(transform.localScale.x, (AudioVisualiser._bandBuffer[_band] * _scaleMultiplier) + _startScale, transform.localScale.z);
+*/
+            Color _color = new Color(AudioVisualiser._audioBandBuffer[_band], AudioVisualiser._audioBandBuffer[_band], AudioVisualiser._audioBandBuffer[_band]);
+            /*if(_color > 0.5)
+            {
+
+            }*/
+           /* _color = _color * 0.5;*/
+            Debug.Log("Emission Color : "+ _color);
+            material.SetColor("_EmissionColor", _color);
+
+        }
+        if (!_useBuffer)
+        {
+/*            transform.localScale = new Vector3(transform.localScale.x, (AudioVisualiser._freqBands[_band] * _scaleMultiplier) + _startScale, transform.localScale.z);
+*/            Color _color = new Color(AudioVisualiser._audioBand[_band], AudioVisualiser._audioBand[_band], AudioVisualiser._audioBand[_band]);
+            material.SetColor("_EmissionColor", _color);
+
+        }
+    }
+}
